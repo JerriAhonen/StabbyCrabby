2f6ff2cf64a478309bfb3de69d9ca7d4e54ef15a
KALLE-PC\Kalle <kalle.vikstrom@cs.tamk.fi>
2018-10-08 19:26:01 +0300

    Fixed Animation masks and texture bug.

--- Assets/Code/PlayerCombo.cs
+++ Assets/Code/PlayerCombo.cs
@@ -6,11 +6,11 @@ public class PlayerCombo : MonoBehaviour
 {
 
     public float fireRate;
+    string[] comboParams;
+    int comboIndex = 0;
+    Animator animator;
+    float resetTimer;
 
-    public string[] comboParams;
-    private int comboIndex = 0;
-    private Animator animator;
-    private float resetTimer;
     void Awake()
     {
         if (comboParams == null || (comboParams != null && comboParams.Length == 0))
@@ -20,29 +20,36 @@ public class PlayerCombo : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && comboIndex < comboParams.Length)
-        {
-            Debug.Log(comboParams[comboIndex] + " triggered");
-            animator.SetTrigger(comboParams[comboIndex]);
-
-            // If combo must not loop
-            //comboIndex++;
-
-            // If combo can loop
-             comboIndex = (comboIndex + 1) % comboParams.Length;
-            
-            resetTimer = 0f;
-        }
         // Reset combo if the user has not clicked quickly enough
         if (comboIndex > 0)
         {
             resetTimer += Time.deltaTime;
-            Debug.Log("reset timer:" + resetTimer);
+           
             if (resetTimer > fireRate)
             {
                 animator.SetTrigger("Reset");
                 comboIndex = 0;
             }
         }
+        if (Input.GetButtonDown("Fire1") && comboIndex < comboParams.Length)
+        {
+            
+           
+            
+            animator.SetTrigger(comboParams[comboIndex]);
+            
+            // If combo must not loop
+            //comboIndex++;
+
+            // If combo can loop
+            comboIndex = (comboIndex + 1) % comboParams.Length;
+            if(comboIndex >= 1)
+            {
+                comboIndex = 1;
+            }
+            resetTimer = 0f;
+        }
+        
     }
+ 
 }
