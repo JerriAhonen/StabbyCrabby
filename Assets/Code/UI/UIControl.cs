d272f47c25855eb0d52b422d2b6f827b4c667087
Cagez <kallevikstrom@hotmail.com>
2018-10-11 15:34:20 +0300

    Added UI stuff

--- Assets/Code/UI/UIControl.cs
+++ Assets/Code/UI/UIControl.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIControl: MonoBehaviour {
 
     public Slider staminaSlider;
+    public Slider specialMoveSlider;
     public InputReader inputReader;
+    public TMP_Text timerText;
+    public TMP_Text comboText;
+    public TMP_Text pointsText;
 
     public int maxStamina;
     int staminaFallRate;
@@ -14,26 +19,50 @@ public class UIControl: MonoBehaviour {
     int staminaRegainRate;
     public int staminaRegainMult;
 
+    public int maxSpecial;
+
     public bool canAttack;
+    public bool specialMoveIsActive;
+
+    float timer;
+    int combo;
+    int points;
+    public float comboResetTime;
 
 	// Use this for initialization
 	void Start () {
 
         inputReader = InputReader.Instance;
+        
         staminaSlider.maxValue = maxStamina;
         staminaSlider.value = maxStamina;
-
+        specialMoveSlider.maxValue = maxSpecial;
+        specialMoveSlider.value = 0;
         staminaFallRate = 1;
         staminaRegainRate = 1;
-
         canAttack = true;
-
+        specialMoveIsActive = false;
+        timer = 0f;
+        combo = 1;
+        points = 0;
+        comboResetTime = 4f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-        if(Input.GetButtonDown("Fire1"))
+
+        Stamina();
+        Timer();
+        ComboMeter();
+        Points();
+        SpecialMove();
+	}
+
+    // Displays current stamina and toggles if the player can attack or not.
+    void Stamina()
+    {
+        
+        if (Input.GetButtonDown("Fire1"))
         {
             staminaSlider.value -= 1;
         }
@@ -43,7 +72,7 @@ public class UIControl: MonoBehaviour {
             staminaSlider.value += Time.deltaTime * staminaRegainMult;
         }
 
-        if(staminaSlider.value >= maxStamina)
+        if (staminaSlider.value >= maxStamina)
         {
             staminaSlider.value = maxStamina;
         }
@@ -58,5 +87,66 @@ public class UIControl: MonoBehaviour {
         {
             canAttack = true;
         }
-	}
+
+    }
+
+    // Displays the game timer.
+    void Timer()
+    {
+        timer += Time.deltaTime;
+
+        timerText.text = timer.ToString("F2");
+       
+    }
+
+    // Update points for killing enemies.
+    void Points()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            points = points + 100;
+        }
+        pointsText.text = "p: " + points.ToString();
+    }
+
+    // Update current combo amount.
+    void ComboMeter()
+    {
+        // Change this to "If you kill something"
+        if(Input.GetButtonDown("Fire1"))
+        {
+            combo = combo + 1;
+            comboResetTime = 4f;
+           
+        }
+        if(combo > 0)
+        {
+            comboResetTime -= Time.deltaTime;
+            if(comboResetTime <= 0)
+            {
+                combo = 0;
+            }
+        }
+        comboText.text = combo.ToString();
+    }
+
+    // Updates the current state of the SpecialMove bar.
+    void SpecialMove()
+    {
+        if (specialMoveSlider.value == maxSpecial)
+        {
+            specialMoveIsActive = true;
+        }
+        else
+        {
+            specialMoveIsActive = false;
+        }
+
+        // Change this to if getting points.
+        if (Input.GetButtonDown("Fire1"))
+        {
+            specialMoveSlider.value += 1;
+        }
+        
+    }
 }
