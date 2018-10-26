210e214ac01957099e5459078d29e24834776c65
Suvi <suvi.kemppainen@cs.tamk.fi>
2018-10-26 06:39:28 +0300

    Generic Health component.

---
+++ Assets/Code/Health.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    private int _currentHealth;
+
+    public int CurrentHealth {
+        get {
+            return _currentHealth;
+        }
+        private set {
+            _currentHealth = value;
+        }
+    }
+
+    private bool _isDead;
+
+    public bool IsDead {
+        get {
+            return _isDead;
+        }
+        private set {
+            _isDead = value;
+        }
+    }
+    
+    public Health(int startingHealth) {
+        CurrentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage) {
+        CurrentHealth = Mathf.Clamp(CurrentHealth - damage, 0, CurrentHealth);
+
+        if (CurrentHealth == 0) {
+            IsDead = true;
+        }
+    }
+
+    public void SetHealth(int health) {
+        if (!IsDead) {
+            CurrentHealth = health;
+        }
+    }
+}
