793ab56e99a9a1fc9a8bd7a0ce6a4c0cf51a9004
JerJerBinks <jerri.ahonen@gmail.com>
2019-01-13 13:43:31 +0200

    Changed GameManager.cs to MainMenu.cs in MainMenu scene.

--- Assets/Code/Management/GameManager.cs
+++ Assets/Code/Management/GameManager.cs
@@ -4,25 +4,9 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
     public static GameManager Instance { get; set; }
-    private int currentSceneIndex;
-
-    // MAIN MENU CAMERA
-
-    /*
-    public GameObject mmCameraPos;
-    public GameObject optionsCameraPos;
-    private Vector3 _targetPosition;
-    private Quaternion _targetRotation;
-    private bool _cameraIsMoving;
-    */
+    
     private InputReader _inputReader;
-    private Animator _cameraAnim;
-
-    // MAIN MENU CANVAS
-
-    public GameObject mainMenuCanvas;
-    public GameObject optionsCanvas;
-
+    
     public GameObject gameUI;
     public GameObject endScreenUI;
     public GameObject pauseMenuUI;
@@ -33,56 +17,23 @@ public class GameManager : MonoBehaviour {
             Instance = this;
         else if (Instance != this)
             Destroy(gameObject);
-
-        DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
     {
-        if (SceneManager.GetActiveScene().buildIndex == 0)  // Main Menu
-        {
-            mainMenuCanvas = GameObject.FindGameObjectWithTag("MainMenuCanvas");
-            optionsCanvas = GameObject.FindGameObjectWithTag("OptionsCanvas");
-            _cameraAnim = Camera.main.GetComponent<Animator>();
-            mainMenuCanvas.SetActive(true);
-            optionsCanvas.SetActive(false);
-        }
-        else if (SceneManager.GetActiveScene().buildIndex == 1)  // Workshop
-        {
-            _inputReader = InputReader.Instance;
-            gameUI = GameObject.FindGameObjectWithTag("GameUI");
-            endScreenUI = GameObject.FindGameObjectWithTag("EndScreenUI");
-            pauseMenuUI = GameObject.FindGameObjectWithTag("PauseMenuUI");
+        _inputReader = InputReader.Instance;
+        gameUI = GameObject.FindGameObjectWithTag("GameUI");
+        endScreenUI = GameObject.FindGameObjectWithTag("EndScreenUI");
+        pauseMenuUI = GameObject.FindGameObjectWithTag("PauseMenuUI");
 
-            gameUI.SetActive(true);
-            endScreenUI.SetActive(false);
-            pauseMenuUI.SetActive(false);
-        }
+        gameUI.SetActive(true);
+        endScreenUI.SetActive(false);
+        pauseMenuUI.SetActive(false);
     }
 
     private void Update()
     {
         if (SceneManager.GetActiveScene().buildIndex == 1 && _inputReader.Esc) Esc();
-    }
-    /// <summary>
-    /// Possibly doesn't work.
-    /// </summary>
-    /// <param name="level"></param>
-    private void OnLevelWasLoaded(int level)
-    {
-        currentSceneIndex = level;
-
-        if (SceneManager.GetActiveScene().buildIndex == 1)  // Workshop
-        {
-            _inputReader = InputReader.Instance;
-            gameUI = GameObject.FindGameObjectWithTag("GameUI");
-            endScreenUI = GameObject.FindGameObjectWithTag("EndScreenUI");
-            pauseMenuUI = GameObject.FindGameObjectWithTag("PauseMenuUI");
-
-            gameUI.SetActive(true);
-            endScreenUI.SetActive(false);
-            pauseMenuUI.SetActive(false);
-        }
     }
 
     public void ChangeScene(int sceneNum)
@@ -94,67 +45,20 @@ public class GameManager : MonoBehaviour {
     {
         Application.Quit();
     }
-
-    public void ChangeMMTab(int tab)
-    {
-        if (tab == 0)   // Main Menu
-        {
-            /*
-            _targetPosition = mmCameraPos.transform.position;
-            _targetRotation = mmCameraPos.transform.rotation;
-
-            _cameraIsMoving = true;
-            */
-
-            _cameraAnim.SetTrigger("ToMainmenu");
-
-            mainMenuCanvas.SetActive(true);
-            optionsCanvas.SetActive(false);
-        }
-        else if (tab == 1)  // Options
-        {
-            /*
-            _targetPosition = optionsCameraPos.transform.position;
-            _targetRotation = optionsCameraPos.transform.rotation;
-
-            _cameraIsMoving = true;
-            */
-
-            _cameraAnim.SetTrigger("ToOptions");
-
-            mainMenuCanvas.SetActive(false);
-            optionsCanvas.SetActive(true);
-        }
-    }
-
+    
     public void Esc()
     {
-        if (currentSceneIndex == 0)  // Main Menu
-        {
-            Quit();
-        }
-        else if (currentSceneIndex == 1)  // Workshop
-        {
-            endScreenUI.SetActive(true);
-        }
+        // Toggle pause menu on off
+
+        endScreenUI.SetActive(true);
+    }
+
+    /// <summary>
+    /// Change the speed at which time passes.
+    /// </summary>
+    /// <param name="scale">1=Normal speed, 0=Paused</param>
+    public void ChangeTimeScale(float scale)
+    {
+        Time.timeScale = scale;
     }
 }
-/*
-    void MoveCamera(Vector3 targetPosition, Quaternion targetRotation)
-    {
-        if (Vector3.Distance(Camera.main.transform.position, targetPosition) > 0.1f)
-        {
-            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position,
-                                                        targetPosition,
-                                                        5f * Time.deltaTime);
-            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation,
-                                                                targetRotation,
-                                                                5f * Time.deltaTime);
-        }
-        else
-        {
-            _cameraIsMoving = false;
-        }
-        
-    }
-    */
---
+++ Assets/Code/Management/MainMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    private InputReader _inputReader;
+    private Animator _cameraAnim;
+    
+    public GameObject mainMenuCanvas;
+    public GameObject optionsCanvas;
+    
+    public enum Tab
+    {
+        MainMenu = 0,
+        Options = 1,
+        Controls = 2,
+        Credits = 3
+    }
+
+    void Start ()
+    {
+        mainMenuCanvas = GameObject.FindGameObjectWithTag("MainMenuCanvas");
+        optionsCanvas = GameObject.FindGameObjectWithTag("OptionsCanvas");
+        _cameraAnim = Camera.main.GetComponent<Animator>();
+        mainMenuCanvas.SetActive(true);
+        optionsCanvas.SetActive(false);
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    public void ChangeMMTab(int tab)
+    {
+        if (tab == (int)Tab.MainMenu)
+        {
+            _cameraAnim.SetTrigger("ToMainmenu");
+
+            mainMenuCanvas.SetActive(true);
+            optionsCanvas.SetActive(false);
+        }
+        else if (tab == (int)Tab.Options)
+        {
+            _cameraAnim.SetTrigger("ToOptions");
+
+            mainMenuCanvas.SetActive(false);
+            optionsCanvas.SetActive(true);
+        }
+    }
+
+    public void ChangeScene(int sceneNum)
+    {
+        SceneManager.LoadSceneAsync(sceneNum);
+    }
+}
